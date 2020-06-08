e910a6ac03508d90408f6c67a4f769231605e9fe
magemasterII <66111204+magemasterII@users.noreply.github.com>
2020-06-07 23:23:32 -0700

    free tinkering spec skills
    Changed Trained requirement on spec tinkering augs to Untrained so all tinkering skills can be specialized from untrained for free

--- Source/ACE.Server/WorldObjects/AugmentationDevice.cs
+++ Source/ACE.Server/WorldObjects/AugmentationDevice.cs
@@ -172,7 +172,7 @@ namespace ACE.Server.WorldObjects
             {
                 var playerSkill = player.GetCreatureSkill(AugTypeHelper.GetSkill(type));
 
-                if (playerSkill.AdvancementClass != SkillAdvancementClass.Trained)
+                if (playerSkill.AdvancementClass != SkillAdvancementClass.Untrained)
                 {
                     player.SendWeenieErrorWithString(WeenieErrorWithString.AugmentationSkillNotTrained, $"You are not able to purchase this augmentation because you are not trained in {playerSkill.Skill.ToSentence()}!");
                     return false;
