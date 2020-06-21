78b2200d0ccc945d3ebe11c340e541d27ae25db1
Randall Flagg <russellfannin0@gmail.com>
2020-06-21 01:27:11 -0700

    [#17] add server config for "turbine_chat_min_level"

--- Source/ACE.Server/Managers/PropertyManager.cs
+++ Source/ACE.Server/Managers/PropertyManager.cs
@@ -554,7 +554,8 @@ namespace ACE.Server.Managers
                 ("rares_max_seconds_between", new Property<long>(5256000, "for rares_real_time: the maximum number of seconds a player can go before a second chance at a rare is allowed on rare eligible creature kills that did not generate a rare")),
                 ("teleport_visibility_fix", new Property<long>(0, "Fixes some possible issues with invisible players and mobs. 0 = default / disabled, 1 = players only, 2 = creatures, 3 = all world objects")),
                 ("ignore_burden_below_character_level", new Property<long>(100, "The minimum character level at which burden will start to apply. Retail defaults to 0.")),
-                ("pk_logout_timer_min_level", new Property<long>(100, "The minimum character level for which the pk logout timer will apply."))
+                ("pk_logout_timer_min_level", new Property<long>(100, "The minimum character level for which the pk logout timer will apply.")),
+                ("turbine_chat_min_level", new Property<long>(1, "Minimum character level to use global chat channels (General, LFG, Roleplay, Trade, Olthoi, Society, Allegience)"))
                 );
 
         public static readonly ReadOnlyDictionary<string, Property<double>> DefaultDoubleProperties =
